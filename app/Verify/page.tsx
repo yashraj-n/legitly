5869ac813e7c4f931d7142960683d3a19b455de2
Gaurav Naresh Sunthwal <70788155+codewithgaurav252005@users.noreply.github.com>
2024-03-09 23:18:27 +0530

    sd

--- app/Verify/page.tsx
+++ app/Verify/page.tsx
@@ -1,31 +1,68 @@
 //@ts-nocheck
 "use client";
 
-import { Box, Button, HStack, Input, VStack } from "@chakra-ui/react";
-
+import {
+  Box,
+  Button,
+  HStack,
+  Input,
+  Text,
+  VStack,
+  useMediaQuery,
+} from "@chakra-ui/react";
+import Image from "next/image";
+import verifyImg from "../Img/verify.png";
 export default function Verify() {
+  const [isGraterthen] = useMediaQuery("(min-width:600px)");
   function handalSubmit(e) {
     e.preventDefault();
   }
+  function handleFileChange() {}
   return (
     <>
-      <form>
-        <VStack w={"100%"} h={"72vh"} justifyContent={"center"} p={2}>
-          <InputsTags inputType={"text"} />
-          <InputsTags inputType={"file"} />
-          <Button className="nextBtn" type="submit" onClick={handalSubmit}>
-            Submit
-          </Button>
-        </VStack>
-      </form>
+      <HStack justifyContent={"space-evenly"} flexWrap={"wrap"}>
+        <Box w={isGraterthen ? "50%" : "100%"}>
+          <form>
+            <Box w={"100%"} p={3} m={3}>
+              <Text mb="8px">Enter</Text>
+              <Input w={"97%"} />
+            </Box>
+            <Box p={3} m={3}>
+              <Text mb="8px">Choose File</Text>
+              <Input
+                type="file"
+                id="fileInput"
+                style={{ display: "none" }}
+                accept="application/pdf"
+                onChange={handleFileChange}
+              />
+              <Button
+                w={"100%"}
+                onClick={() => document.getElementById("fileInput").click()}
+              >
+                Choose File
+              </Button>
+            </Box>
+            <VStack>
+              <Button className="nextBtn" type="submit">
+                Submit
+              </Button>
+            </VStack>
+          </form>
+        </Box>
+        <Box w={isGraterthen ? "40%" : "100%"}>
+          <Image src={verifyImg} alt="verifyImg" />
+        </Box>
+      </HStack>
     </>
   );
 }
 
-function InputsTags({ inputType, handalChange }) {
+function InputsTags({ inputType, lable, handalChange }) {
   return (
-    <HStack justifyContent={"center"} w={"100%"}>
-      <Input type={inputType} m={3} onChange={handalChange} w={"90%"} />
-    </HStack>
+    <Box w={"100%"} p={3} m={3}>
+      <Text mb="8px">{lable}</Text>
+      <Input w={"100%"} />
+    </Box>
   );
 }
